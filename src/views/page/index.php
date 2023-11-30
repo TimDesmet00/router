4bc7455f3b343e16b6853a4323542ca982b0b699
tim <timdesmet00@gmail.com>
2023-11-30 11:15:42 +0100

    ajout de l'auteur dans la page index

--- src/views/page/index.php
+++ src/views/page/index.php
@@ -6,7 +6,7 @@
     <h1>Articles</h1>
     <ul>
         <?php foreach ($articles as $article) : ?>
-            <li><a href="./index.php?page=page-show&id=<?= $article->id ?>"><?= $article->title ?> (<?= $article->formatPublishDate() ?>)</a></li>
+            <li><a href="./index.php?page=page-show&id=<?= $article->id ?>"><?= $article->title ?> (Par: <?= $article->author_first_name ?> <?= $article->author_last_name ?> le:<?= $article->formatPublishDate() ?>)</a></li>
         <?php endforeach; ?>
     </ul>
 </section>
